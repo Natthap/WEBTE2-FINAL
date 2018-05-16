c53bf63b33324a0e390573a0fd4b199b50ebf49f
Martin Žikla <m.zikla@gmail.com>
2018-05-16 19:10:42 +0200

    pridane vytvorenie uzivatelskej trasy, zmena active trasy a ziskanie vsetkych stras

--- php/services/ServiceRoutes.php
+++ php/services/ServiceRoutes.php
@@ -6,6 +6,41 @@
  * Time: 13:52
  */
 
-function createRoute() {
-    
+function createUserRoute($db, $routeData) {
+    $sql = "INSERT INTO routes (FK_members, name, active, geojson, type)
+            VALUES ('"
+            .$routeData["userID"]."', '".$routeData["name"]."', '"
+            .$routeData["active"]."', '".$routeData["geojson"]."', '"
+            .$routeData["type"]
+            ."')";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+
 }
+
+function updateActiveUserRoute($db, $routeData) {
+    $sql = "UPDATE routes
+            SET active='".$routeData["active"]."'
+            WHERE id='".$routeData["userID"]."'";
+
+    echo $sql;
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+}
+
+function getAllUserRoutes($db) {
+    $sql = "SELECT * FROM routes";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($result);
+}
+?>
