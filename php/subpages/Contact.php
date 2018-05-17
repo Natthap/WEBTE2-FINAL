2d32aaeb7c7288a934497e2b823dc94785c9bcd5
Homees <galis.filip@gmail.com>
2018-05-17 20:24:21 +0200

    praca na index.php

--- php/subpages/Contact.php
+++ php/subpages/Contact.php
@@ -19,15 +19,20 @@ $title = 'Kontakt';
 //include header template
 require('layout/header.php');
 require("layout/Menu.php");
+?>
 
-echo '<div class="container">';
-if ($_SESSION['personType'] == 2) {
+<div class="container">
+    <div class="contact_form wow fadeInLeft">
+        <form class="" role="form" method="post" action="" autocomplete="off">
+            <div class="form-group"><input type="text" name="name" class="wp-form-control wpcf7-text" placeholder="Vaše meno"></div>
+            <div class="form-group"><input type="mail" name="email" class="wp-form-control wpcf7-email" placeholder="Emailová adresa"></div>
+            <div class="form-group"><input type="text" name="subject" class="wp-form-control wpcf7-text" placeholder="Predmet"></div>
+            <div class="form-group"><textarea name="message" class="wp-form-control wpcf7-textarea" cols="30" rows="10" placeholder="Čo by ste nám chceli povedat?"></textarea></div>
+            <div class="form-group"><input type="submit" name="submit" value="Poslať" class="wpcf7-submit"></div>
+        </form>
+    </div>
+<div>
 
-}
-if ($_SESSION['personType'] == 1) {
-
-}
-echo '<div>';
-
+<?php
 //include header template
 require('layout/footer.php');
