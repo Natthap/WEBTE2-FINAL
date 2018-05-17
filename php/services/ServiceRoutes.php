23c74fcaee31904aa2696ecfb34aa54e36b52ac3
Martin Žikla <m.zikla@gmail.com>
2018-05-17 15:58:13 +0200

    pridane ostatne funkcie

--- php/services/ServiceRoutes.php
+++ php/services/ServiceRoutes.php
@@ -23,17 +23,15 @@ function createUserRoute($db, $routeData) {
 function updateActiveUserRoute($db, $routeData) {
     $sql = "UPDATE routes
             SET active='".$routeData["active"]."'
-            WHERE id='".$routeData["userID"]."'";
-
-    echo $sql;
+            WHERE id='".$routeData["routeID"]."'";
 
     $stmt = $db->prepare($sql);
 
     $stmt->execute();
 }
 
-function getAllUserRoutes($db) {
-    $sql = "SELECT * FROM routes";
+function getAllUserRoutes($db, $userID) {
+    $sql = "SELECT * FROM routes WHERE FK_members='".$userID."'";
 
     $stmt = $db->prepare($sql);
 
@@ -41,6 +39,65 @@ function getAllUserRoutes($db) {
 
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($result);
+    return $result;
 }
+
+function getAllPublicRoutes($db) {
+    $sql = "SELECT * FROM routes WHERE type='0'";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+function createTeamRelayRoute($db, $routeData) {
+    $sql = "INSERT INTO routes_relay (FK_team, name, active, geojson)
+            VALUES ('"
+        .$routeData["teamID"]."', '".$routeData["name"]."', '"
+        .$routeData["active"]."', '".$routeData["geojson"]."', '"
+        ."')";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+}
+
+function updateTeamRelayRoute($db, $routeData) {
+    $sql = "UPDATE routes_relay
+            SET active='".$routeData["active"]."'
+            WHERE ='".$routeData["routeID"]."'";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+}
+
+function getAllTeamRelayRoutes($db, $teamID) {
+    $sql = "SELECT * FROM routes_ralay WHERE FK_team='".$teamID."'";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+function getAllRelayRoutes($db) {
+    $sql = "SELECT * FROM routes_ralay";
+
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
 ?>
