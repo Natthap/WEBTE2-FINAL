2ed0b04db9a4ff551f0724b4ebc815df4a8c5d53
Martin Žikla <m.zikla@gmail.com>
2018-05-18 19:45:56 +0200

    opravene dorobene

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,9 +1,11 @@
     var map;
-
     var request;
     var markers = [];
     var subRoutes = [];
     var route;
+    var directions;
+    var renderer;
+    var polylines = [];
 
     function initialize() {
         var latlng = new google.maps.LatLng(48.750086, 19.669258);
@@ -13,83 +15,147 @@
             mapTypeId: 'roadmap'
         };
         map = new google.maps.Map(document.getElementById("mapDiv"), myOptions);
-
-        google.maps.event.addListener(map, 'click', function(event) {
-            placeMarker(event.latLng);
-            if(markers.length == 2) {
-
-                var org = new google.maps.LatLng ( markers[0].getPosition().lat(), markers[0].getPosition().lng());
-                var dest = new google.maps.LatLng ( markers[1].getPosition().lat(), markers[1].getPosition().lng());
-
-                google.maps.event.trigger(markers[1], 'click');
-                google.maps.event.trigger(markers[0], 'click');
-
-                request = {
-                    origin: org,
-                    destination: dest,
-                    travelMode: google.maps.DirectionsTravelMode.WALKING
-                };
-
-                addRoute(request);
-            }
+        directions  = new google.maps.DirectionsService();
+        renderer = new google.maps.DirectionsRenderer({
+            suppressPolylines: true
         });
+        
+        if(!route) {
+            google.maps.event.addListener(map, 'click', function (event) {
+                placeMarker(event.latLng);
+                if (markers.length == 2) {
 
-        function placeMarker(location) {
-            if(!request) {
-                var marker = new google.maps.Marker({
-                    position: location,
-                    map: map,
-                    indexOf: markers.length
-                });
-                if(subRoutes || route) {
-                    if(subRoutes) {
-                        request1 = {
-                            origin: new google.maps.LatLng(subroutes[subRoutes.length-1].destination.getLat(), subroutes[subRoutes.length-1].destination.getLng()),
-                            destination: new google.maps.LatLng (marker.getPosition().getLat(), marker.getPosition().getLng()),
-                            travelMode: google.maps.DirectionsTravelMode.WALKING
-                        };
-                        addRoute(request1);
-                        request = request1;
-                    } else if(route) {
-                        request1 = {
-                            origin: new google.maps.LatLng(route.origin.getLat(), route.origin.getLng()),
-                            destination: new google.maps.LatLng (marker.getPosition().getLat(), marker.getPosition().getLng()),
-                            travelMode: google.maps.DirectionsTravelMode.WALKING
-                        };
-                        addRoute(request1);
-                        request = request1;
-                    }
+                    var org = new google.maps.LatLng(markers[0].getPosition().lat(), markers[0].getPosition().lng());
+                    var dest = new google.maps.LatLng(markers[1].getPosition().lat(), markers[1].getPosition().lng());
+
+                    google.maps.event.trigger(markers[1], 'click');
+                    google.maps.event.trigger(markers[0], 'click');
+
+                    request = {
+                        origin: org,
+                        destination: dest,
+                        travelMode: google.maps.DirectionsTravelMode.WALKING
+                    };
+                    addRoute(request, 1);
                 }
-                google.maps.event.addListener(marker, 'click', function (event) {
-                    this.setMap(null);
-                    removeMarker(marker);
-                });
-                markers.push(marker);
-            }
-        }
-
-        function removeMarker(marker) {
-            markers.splice(marker.indexOf, 1);
+            });
         }
     }
 
-    function addRoute(request) {
-        var rendererOptions = {map: map};
-        directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-
-        directionsService = new google.maps.DirectionsService();
-        directionsService.route(request, function (response, status) {
+    function addRoute(request, value) {
+        directions.route(request, function(response, status) {
             if (status == google.maps.DirectionsStatus.OK) {
-                directionsDisplay.setDirections(response);
-                $('#gps').val(JSON.stringify(request));
+                renderer.setDirections(response);
+                renderer.setMap(map);
+                var polylineOptions = {
+                    strokeColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+                    strokeOpacity: 1,
+                    strokeWeight: 4
+                };
+                if(value == 0) {
+                    polylineOptions.strokeOpacity = 0.2;
+                } else {
+                    $('#gps').val(JSON.stringify(request));
+                }
+                renderDirectionsPolylines(response,polylineOptions, value);
+                console.log(renderer.getDirections());
+            } else {
+                renderer.setMap(null);
+                renderer.setPanel(null);
             }
-            else
-                alert('failed to get directions');
         });
+    }
+
+
+    function renderDirectionsPolylines(response,polylineOptions, value) {
+        var legs = response.routes[0].legs;
+        for (i = 0; i < legs.length; i++) {
+            var steps = legs[i].steps;
+            for (j = 0; j < steps.length; j++) {
+                var nextSegment = steps[j].path;
+                var stepPolyline = new google.maps.Polyline(polylineOptions);
+                for (k = 0; k < nextSegment.length; k++) {
+                    stepPolyline.getPath().push(nextSegment[k]);
+                }
+                if(value == 1) {
+                    polylines.push(stepPolyline);
+                }
+                stepPolyline.setMap(map);
+                // route click listeners, different one on each step
+                google.maps.event.addListener(stepPolyline, 'click', function(evt) {
+                    placeMarker(evt.latLng);
+                })
+            }
+        }
+    }
+
+    function placeMarker(location) {
+        if(!request) {
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                indexOf: markers.length
+            });
+            if(subRoutes.length != 0 || route) {
+                if(subRoutes.length != 0) {
+                    request1 = {
+                        origin: new google.maps.LatLng(subRoutes[subRoutes.length-1].destination.getLat(), subRoutes[subRoutes.length-1].destination.getLng()),
+                        destination: new google.maps.LatLng (marker.getPosition().lat(), marker.getPosition().lng()),
+                        travelMode: google.maps.DirectionsTravelMode.WALKING
+                    };
+                    addRoute(request1, 1);
+                    request = request1;
+                } else if(route) {
+                    request1 = {
+                        origin: new google.maps.LatLng(route.origin.lat, route.origin.lng),
+                        destination: new google.maps.LatLng (marker.getPosition().lat(), marker.getPosition().lng()),
+                        travelMode: google.maps.DirectionsTravelMode.WALKING
+                    };
+                    addRoute(request1, 1);
+                    request = request1;
+                }
+                marker.setMap(null);
+                return;
+            }
+            google.maps.event.addListener(marker, 'click', function (event) {
+                this.setMap(null);
+                removeMarker(marker);
+            });
+            markers.push(marker);
+        }
+    }
+
+    function removeMarker(marker) {
+        markers.splice(marker.indexOf, 1);
     }
 
     function clearSubRoute() {
-        //nejak odstranit cestu
+        for (var i=0; i<polylines.length; i++) {
+                    polylines[i].setMap(null);
+                }
+                polylines = [];
+        $('#gps').val("");
+
+            if(subRoutes.length != 0) {
+                request1 = {
+                    origin: new google.maps.LatLng(subRoutes[subRoutes.length-1].origin.getLat(), subRoutes[subRoutes.length-1].origin.getLng()),
+                    destination: new google.maps.LatLng(subRoutes[subRoutes.length-1].destination.getLat(), subRoutes[subRoutes.length-1].destination.getLng()),
+                    travelMode: google.maps.DirectionsTravelMode.WALKING
+                };
+
+            } else if(route) {
+                request1 = {
+                    origin: new google.maps.LatLng(route.origin.lat, route.origin.lng),
+                    destination: new google.maps.LatLng (route.destination.lat, route.destination.lng),
+                    travelMode: google.maps.DirectionsTravelMode.WALKING
+                };
+            }
+
+        directions.route(request1, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                renderer.setDirections(response);
+            }
+            });
     }
 
 
